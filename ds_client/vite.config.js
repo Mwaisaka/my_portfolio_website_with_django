50f1838efd82ce076e907b2938f0add68ea11b79
Frank <frankkashere@gmail.com>
2024-11-24 15:33:06 +0300

    Updated backend code 2

--- ds_client/vite.config.js
+++ ds_client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   // root: './client',
   plugins: [react()],
   build: {
-    outDir: '../server/static',  // Correct output directory
+    outDir: '../ds_server/static',  // Correct output directory
     emptyOutDir: true,  // Clear output directory before building
     chunkSizeWarningLimit: 100000 // Set the chunk size warning limit to 100000 bytes (100 kB)
   },
